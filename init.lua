dfeea25030163e2b81331d42aeed23343ecba72b
Stephen Doyle <stephen.doyle@intel.com>
2024-05-20 10:14:32 +0000

    feat: Add auto-format for protobufs

--- init.lua
+++ init.lua
@@ -699,6 +699,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        proto = { 'buf' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
@@ -836,6 +837,7 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'vim'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
