580b6c5c67030f1ae4648691178bdb00b06b221d
Stephen Doyle <stephendoyle75@gmail.com>
2024-06-05 22:09:00 +0100

    Add fzf-lua keymaps

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,14 +4,5 @@
 -- See the kickstart.nvim README for more information
 return {
   'shaunsingh/nord.nvim',
-  {
-    'ibhagwan/fzf-lua',
-    -- optional for icon support
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      -- calling `setup` is optional for customization
-      require('fzf-lua').setup {}
-    end,
-  },
   require('lspconfig').bufls.setup {},
 }
