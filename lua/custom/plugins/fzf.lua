ce9cef91a8c890e212c7f81b07d4b3435d231efd
Stephen Doyle <stephendoyle75@gmail.com>
2024-06-05 22:09:00 +0100

    Add fzf-lua keymaps

---
+++ lua/custom/plugins/fzf.lua
@@ -0,0 +1,17 @@
+return {
+  'ibhagwan/fzf-lua',
+  -- optional for icon support
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    -- calling `setup` is optional for customization
+    require('fzf-lua').setup {}
+  end,
+  vim.keymap.set('n', '<c-\\>', "<cmd>lua require('fzf-lua').buffers()<CR>", {}),
+  vim.keymap.set('n', '<c-k>', "<cmd>lua require('fzf-lua').builtin()<CR>", {}),
+  vim.keymap.set('n', '<c-p>', "<cmd>lua require('fzf-lua').files()<CR>", {}),
+  vim.keymap.set('n', '<c-l>', "<cmd>lua require('fzf-lua').live_grep_glob()<CR>", {}),
+  vim.keymap.set('n', '<c-g>', "<cmd>lua require('fzf-lua').grep_project()<CR>", {}),
+  vim.keymap.set('n', '<F1>', "<cmd>lua require('fzf-lua').help_tags()<CR>", {}),
+
+  require('fzf-lua').utils.info '|<C-\\> buffers|<C-p> files|<C-g> grep|<C-l> live grep|<C-k> builtin|<F1> help|',
+}
