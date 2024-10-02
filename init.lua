a6346218195fae683988fc447ffaf34fc26f9cdf
Stephen Doyle <stephen.doyle@intel.com>
2024-10-02 09:41:57 +0100

    feat: Change color scheme for work env

--- init.lua
+++ init.lua
@@ -846,9 +846,9 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-night'
       -- vim.cmd.colorscheme 'vim'
-      vim.cmd.colorscheme 'default'
+      -- vim.cmd.colorscheme 'default'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
