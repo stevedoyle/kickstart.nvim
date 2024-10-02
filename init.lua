e725118e22be4845b6ce5152afba624beca63d44
Stephen Doyle <stephendoyle75@gmail.com>
2024-10-02 09:37:52 +0100

    feat: add key binding for expanding errors
    Added <leader>e for displaying expanded inline LSP errors.

--- init.lua
+++ init.lua
@@ -553,6 +553,9 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          -- Open a floating window with the diagnostic list
+          map('<leader>e', vim.diagnostic.open_float, '[E]rrors')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
