811bc13d81b903ce4598a2c3ad851a83e834a785
Stephen Doyle <stephen.doyle@intel.com>
2026-09-07 14:50:29 +0100

    Add bullets.nvim plugin

---
+++ lua/custom/plugins/bullets.lua
@@ -0,0 +1,5 @@
+-- Continuing, renumbering, promoting, demoting, and toggling bullet lists
+--  See https://github.com/bullets-vim/bullets.nvim
+vim.pack.add { 'https://github.com/bullets-vim/bullets.nvim' }
+
+require('bullets').setup {}
